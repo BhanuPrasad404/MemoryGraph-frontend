605729919ac6360bc8951285fcfcf819f137b8d3
BhanuPrasad404 <bhanuprasadgummidelli@gmail.com>
2026-02-19 14:43:00 +0530

    added 1s delay

--- src/components/DashboardPageInner.tsx
+++ src/components/DashboardPageInner.tsx
@@ -64,7 +64,7 @@ export default function DashboardPage() {
             }
 
             setTokenLoaded(true);
-        }, 0);
+        }, 1000);
 
         return () => clearTimeout(timer);
     }, [hydrated, router]);
